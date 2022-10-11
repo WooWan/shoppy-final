c9632a62faf1c66bfb2bf556fbb9a662ca92bd90
wooho <woohobi@gmail.com>
2022-10-11 16:12:31 +0900

    컴포넌트 테스트

--- alie/test-code/habit-tracker/src/habit_presenter.js
+++ alie/test-code/habit-tracker/src/habit_presenter.js
@@ -1,11 +1,19 @@
 export default class HabitPresenter {
-  constructor(habit) {
-    this.habit = habit;
+  // maxHabits을 외부에서 받아와서 사용할 수 있도록 한다.
+  // test code를 작성할 때, 임의의 값을 넣어서 테스트할 수 있다.
+  // ex) max habits = 100 을 설정하면 테스트할 때 100개의 habit을 만들어야 함.
+  constructor(habits, maxHabits) {
+    this.habits = habits;
+    this.maxHabits = maxHabits;
+  }
+
+  getHabits() {
+    return this.habits;
   }
 
   increment(habit, update) {
     this.habits = this.habits.map((item) => {
-      if (item.id === this.habit.id) {
+      if (item.id === habit.id) {
         return { ...habit, count: habit.count + 1 };
       }
       return item;
@@ -16,7 +24,7 @@ export default class HabitPresenter {
   decrement(habit, update) {
     this.habits = this.habits.map((item) => {
       if (item.id === habit.id) {
-        const count = habit.count - 1;
+        const count = item.count - 1;
         return { ...habit, count: count < 0 ? 0 : count };
       }
       return item;
@@ -25,6 +33,9 @@ export default class HabitPresenter {
   }
 
   addHabit(habit, update) {
+    if (this.habits.length === this.maxHabits) {
+      throw new Error(`습관의 최대 개수는 ${this.maxHabits}개 입니다.`);
+    }
     this.habits = [...this.habits, habit];
     update(this.habits);
   }
@@ -33,7 +44,7 @@ export default class HabitPresenter {
     this.habits = this.habits.filter((item) => item.id !== habit.id);
     update(this.habits);
   }
-  resetHabit(habit, update) {
+  resetHabit(update) {
     this.habits = this.habits.map((habit) => {
       if (habit.count !== 0) {
         return { ...habit, count: 0 };
--- alie/test-code/habit-tracker/src/index.js
+++ alie/test-code/habit-tracker/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
-import '@fortawesome/fontawesome-free/js/all.js';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./app";
+import HabitPresenter from "./habit_presenter";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const habitPresenter = new HabitPresenter([
+  { id: 1, name: "Reading", count: 0 },
+  { id: 2, name: "Running", count: 0 },
+  { id: 3, name: "Coding", count: 0 },
+]);
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App habitPresenter={habitPresenter} />);
